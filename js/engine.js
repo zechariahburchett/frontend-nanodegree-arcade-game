e0d9323f91457d103abcfe3785978049a2dd5d4d
Zechariah Burchett <zechariah.burchett@gmail.com>
2018-08-14 23:43:13 -0400

    added scoreboard for lives and player score

--- js/engine.js
+++ js/engine.js
@@ -9,7 +9,7 @@
  * drawn but that is not the case. What's really happening is the entire "scene"
  * is being drawn over and over, presenting the illusion of animation.
  *
- * This engine makes the canvas' context (ctx) object globally available to make 
+ * This engine makes the canvas' context (ctx) object globally available to make
  * writing app.js a little simpler to work with.
  */
 
@@ -79,7 +79,7 @@ var Engine = (function(global) {
      */
     function update(dt) {
         updateEntities(dt);
-        // checkCollisions();
+        //checkCollisions();
     }
 
     /* This is called by the update function and loops through all of the
@@ -117,7 +117,7 @@ var Engine = (function(global) {
             numRows = 6,
             numCols = 5,
             row, col;
-        
+
         // Before drawing, clear existing canvas
         ctx.clearRect(0,0,canvas.width,canvas.height)
 
@@ -154,6 +154,22 @@ var Engine = (function(global) {
         });
 
         player.render();
+
+        //scoreboard keeps track of scores
+        function drawScore() {
+            ctx.font = "16px Arial";
+            ctx.fillStyle = "#33cc33";
+            ctx.fillText("Score: "+score, 8, 20);
+        }
+        drawScore();
+
+        //keeps track of lives
+        function drawLives() {
+            ctx.font = "16px Arial";
+            ctx.fillStyle = "#ff3333";
+            ctx.fillText("Lives: "+lives, 442, 20);
+        }
+        drawLives();
     }
 
     /* This function does nothing but it could have been a good place to
@@ -182,4 +198,5 @@ var Engine = (function(global) {
      * from within their app.js files.
      */
     global.ctx = ctx;
+
 })(this);
